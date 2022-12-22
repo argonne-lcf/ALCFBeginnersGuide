fe73a24b6c0cc6ba2985636d667c9e7472594b09
Taylor Childers <taylorchilders@gmail.com>
2022-12-22 13:29:13 +0000

    adding first script

---
+++ polaris/submit_scripts/00_hello_world.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -l select=1
+#PBS -l walltime=00:30:00
+#PBS -q debug
+#PBS -l filesystems=home
+#PBS -A <project-name>
+#PBS -o logs/
+#PBS -e logs/
+
+module load gcc/11.2.0
+
+GPUS_PER_NODE=4
+
+mpiexec -n $GPUS_PER_NODE -ppn $GPUS_PER_NODE echo Hello World
